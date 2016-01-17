7c9a7c7340ca732384db2eaf198f0aede90f3ebf
maharatha <maharatha@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-01-17 13:26:24 +0000

    Adding the Abpaudit migration. This is change Abp did it not we.

---
+++ CPASCOMMERCIAL/CAPS.CORPACCOUNTING.EntityFramework/Migrations/201601171314153_Modify_AbpAuditLogs.cs
@@ -0,0 +1,18 @@
+namespace CAPS.CORPACCOUNTING.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Modify_AbpAuditLogs : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.AbpAuditLogs", "CustomData", c => c.String(maxLength: 2000));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.AbpAuditLogs", "CustomData", c => c.String());
+        }
+    }
+}

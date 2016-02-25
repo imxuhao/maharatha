2bd74ea55429e5deeb77b7302cf1bd8893d5f6c5
maharatha <maharatha@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-02-25 00:54:56 +0000

    Added Prefix CAPS_E to identify the enum tables and switched off the split word

--- CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.EntityFramework/Migrations/Seed/EnumGenerator.cs
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.EntityFramework/Migrations/Seed/EnumGenerator.cs
@@ -11,7 +11,7 @@ namespace CAPS.CORPACCOUNTING.Migrations.Seed
     /// <summary>
     /// EnumGenerator is a class which will create Enum tables in the database for refrence purpose only along with Foreign Key Relationship
     /// </summary>
-   public  class EnumGenerator :DropCreateDatabaseAlways<CORPACCOUNTINGDbContext>
+    public class EnumGenerator : DropCreateDatabaseAlways<CORPACCOUNTINGDbContext>
     {
         private readonly CORPACCOUNTINGDbContext _context;
 
@@ -27,12 +27,17 @@ namespace CAPS.CORPACCOUNTING.Migrations.Seed
         public void ExampleOfUsingApply()
         {
             var enumToLookup = new EnumToLookup();
-               // enumToLookup.NameFieldLength = 42; // optional, example of how to override default values
+            // enumToLookup.NameFieldLength = 42; // optional, example of how to override default values
+            //This will prevent spliiting the words
+            enumToLookup.SplitWords = false;
 
-                // This would normally be run inside either a db initializer Seed()
-                // or the migration Seed() method which both provide access to a context.
-                enumToLookup.Apply(_context);
-            
+            //Table prefix
+            enumToLookup.TableNamePrefix = "CAPS_E";
+
+            // This would normally be run inside either a db initializer Seed()
+            // or the migration Seed() method which both provide access to a context.
+            enumToLookup.Apply(_context);
+
         }
 
     }

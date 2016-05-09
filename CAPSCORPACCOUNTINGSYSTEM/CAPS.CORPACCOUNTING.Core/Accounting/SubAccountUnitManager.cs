2b8b0f2bfe2edb707ce3b6e68d1483f3071e7ad5
maharatha <maharatha@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-05-09 05:50:22 +0000

    Adding ExtJS UI Documentation

--- CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Core/Accounting/SubAccountUnitManager.cs
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Core/Accounting/SubAccountUnitManager.cs
@@ -6,16 +6,23 @@ using Abp.Zero;
 using System.Threading.Tasks;
 using Abp.UI;
 using System.Linq;
+using System.Linq.Expressions;
+using Abp.Extensions;
+using LinqKit;
+using CAPS.CORPACCOUNTING.Security;
 
 namespace CAPS.CORPACCOUNTING.Accounting
 {
-  public class SubAccountUnitManager : DomainService
+    public class SubAccountUnitManager : DomainService
     {
         protected IRepository<SubAccountUnit, long> SubAccountUnitRepository { get; }
 
-        public SubAccountUnitManager(IRepository<SubAccountUnit, long> subAccountUnitRepository)
+        protected IRepository<SubEntityAccessRestrictionUnit, long> SubEntityAccessRestrictionUnit { get; }
+
+        public SubAccountUnitManager(IRepository<SubAccountUnit, long> subAccountUnitRepository, IRepository<SubEntityAccessRestrictionUnit, long> subentityAccessRepository)
         {
             SubAccountUnitRepository = subAccountUnitRepository;
+            SubEntityAccessRestrictionUnit = subentityAccessRepository;
             LocalizationSourceName = AbpZeroConsts.LocalizationSourceName;
         }
 
@@ -63,6 +70,27 @@ namespace CAPS.CORPACCOUNTING.Accounting
                     }
                 }
             }
+        }
+
+        public Expression<Func<SubAccountUnit, bool>> AccessPredicateByAccount(int accountId)
+        {
+            // return prod => !prod.Discontinued && prod.Purchases.Where(purch => purch.Date > DateTime.Now.AddDays(-30)).Count() >= minPurchases;
+            var predicate = PredicateBuilder.False<SubAccountUnit>();
+
+            if (SubEntityAccessRestrictionUnit.GetAll().Any(p => p.AccountId == accountId && p.UserId == null && p.SubAccountId != null))
+            {
+                var subAccountListInts = SubEntityAccessRestrictionUnit.GetAll().Where(p => p.AccountId == accountId && p.UserId == null && p.SubAccountId != null).ToList();
+                // int[] subAccountListInts = new int[] { 1, 2, 3, 4 };
+
+                predicate = subAccountListInts.Select(p => p.SubAccountId).Select(l =>
+                {
+                    if (l != null) return (long) l;
+                    return 0;
+                }).Aggregate(predicate, (current, subAccount) => current.Or(p => p.Id == subAccount));
+
+                return predicate;
+            }
+            return p => p.IsProjectSubAccount;
         }
     }
 }

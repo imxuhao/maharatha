be037ecb8f58aed1eed9674fe319110949752217
kshahu <kshahu@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-06-23 12:50:59 +0000

    added autofile of roll up combo

---
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/ExtWeb/Chaching/app/store/utilities/autofill/RollupAccountListStore.js
@@ -0,0 +1,23 @@
+﻿Ext.define('Chaching.store.utilities.autofill.RollupAccountListStore', {
+    extend: 'Chaching.store.base.BaseStore',
+    model : 'Chaching.model.financials.accounts.AccountsModel',
+    proxy: {
+        type: 'chachingProxy',
+        actionMethods: { create: 'POST', read: 'POST', update: 'POST', destroy: 'POST' },
+        extraParams: {
+            Id: null
+        },
+        urlToGetRecordById: abp.appPath + 'api/services/app/accountUnit/GetAccountById',
+        api: {
+            read: abp.appPath + 'api/services/app/accountUnit/GetRollupAccountsList',
+            create: abp.appPath + 'api/services/app/accountUnit/CreateAccountUnit',
+            update: abp.appPath + 'api/services/app/accountUnit/UpdateAccountUnit',
+            destroy: abp.appPath + 'api/services/app/accountUnit/DeleteAccountUnit'
+        },
+        reader: {
+            type: 'json',
+            rootProperty: 'result'
+        }
+    },
+    idPropertyField: 'accountId'
+});

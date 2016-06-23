fa029776b3f3500737ebf9eca60ddfb4f32ee079
sgelli <sgelli@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-06-23 11:55:43 +0000

    JournalTransactionDetailGridController.js split functionality changes

--- CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/ExtWeb/Chaching/classic/src/view/financials/journals/JournalTransactionDetailGridController.js
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/ExtWeb/Chaching/classic/src/view/financials/journals/JournalTransactionDetailGridController.js
@@ -22,6 +22,99 @@ Ext.define('Chaching.view.financials.journals.JournalTransactionDetailGridContro
             }
             detailStore.remove(record);
         }
-    }
-    
+    },
+    onSplitClicked: function (field) {
+        var me = this,
+           view = me.getView(),
+           gridStore = view.getStore();
+        var isInlineSplit = false,
+            editor = undefined,
+            cellRecordIndex = undefined;
+        if (field.xtype === "numberfield") {
+            editor = field.up();
+            if (editor && editor.context) {
+                cellRecordIndex = editor.context.record;
+                isInlineSplit = true;
+            }
+        }
+
+        var multiplyOfField = view.down('numberfield[itemId=multiplyOf]'), multiplyOf = multiplyOfField.getValue();
+        if (isInlineSplit && cellRecordIndex) {
+            view.getSelectionModel().select(cellRecordIndex);
+            multiplyOfField = field;
+            multiplyOf = multiplyOfField.getValue();
+            if (editor && editor.editingPlugin) {
+                var cellEditing = editor.editingPlugin;
+                cellEditing.completeEdit();
+            }
+        }
+        var selectionModel = view.getSelectionModel(),
+            selectedRecords = selectionModel.getSelection();
+        if (view.isInViewMode) return;
+
+        if (selectedRecords && selectedRecords.length === 1) {
+            var parentRecord = selectedRecords[0],
+                parentIndex = gridStore.indexOf(parentRecord);
+            if (parentRecord.get('isAccountingItemSplit')) {
+                abp.notify.info(app.localize('AlreadySplit'), app.localize('ValidationFailed'));
+                return;
+            }
+            if (!parentRecord.get('amount')) {
+                abp.notify.info(app.localize('EnterAmountToSplit'), app.localize('ValidationFailed'));
+                return;
+            }
+
+            var modelClass = gridStore.getModel(),
+                className = modelClass.$className;
+            if (multiplyOfField.validate()) {
+                //var amount = parentRecord.get('amount') / multiplyOf,
+                //firstValue = (Math.floor(amount * 100) / 100),
+                //decimalAmount = (parentRecord.get('amount') - (firstValue * multiplyOf)).toFixed(2);
+                //parentRecord.set('amount', +firstValue.toFixed(2));
+                parentRecord.set('isAccountingItemSplit', true);
+                var lastUsedSplitGroupCls = view.lastUsedSplitGroupCls,
+                    splitGroupsCls = Chaching.utilities.ChachingGlobals.splitGroupCls;
+
+                if (!lastUsedSplitGroupCls) {
+                    lastUsedSplitGroupCls = splitGroupsCls[0];
+                    view.lastUsedSplitGroupCls = lastUsedSplitGroupCls;
+                } else {
+                    var indexOfLastGroupCls = splitGroupsCls.indexOf(lastUsedSplitGroupCls);
+                    if (indexOfLastGroupCls + 1 < splitGroupsCls.length) {
+                        lastUsedSplitGroupCls = splitGroupsCls[indexOfLastGroupCls + 1];
+                        view.lastUsedSplitGroupCls = lastUsedSplitGroupCls;
+                    } else {
+                        lastUsedSplitGroupCls = splitGroupsCls[0];
+                        view.lastUsedSplitGroupCls = lastUsedSplitGroupCls;
+                    }
+                }
+                parentRecord.set('SplitGroupCls', lastUsedSplitGroupCls);
+                Ext.suspendLayouts();
+                for (var i = 1; i < multiplyOf; i++) {
+                    var rec = Ext.create(className);
+                    Ext.apply(rec.data, parentRecord.data);
+                    parentRecord.get('accountingItemId') === 0 ? rec.set('SplitAccountingItemId', parentRecord.get('creditAccountingItemId')) : rec.set('SplitAccountingItemId', parentRecord.get('accountingItemId'));
+                    rec.set('accountingItemId', 0);
+                    rec.set('creditAccountingItemId', 0);
+                    rec.set('debitAccountingItemId', null);
+                    rec.set('isAccountingItemSplit', true);
+                    rec.set('amount', 0);
+                    rec.set('creatorUserId', null);
+                    rec.set('lastModifierUserId', null);
+                    rec.set('creationTime', null);
+                    rec.set('lastModificationTime', null);
+                    rec.set('parentRec', parentRecord);
+                    gridStore.insert(parentIndex + 1, rec);
+                }
+                Ext.resumeLayouts();
+                //parentRecord.set('amount', (firstValue + +decimalAmount).toFixed(2));
+            }
+        } else {
+            if (selectedRecords && selectedRecords.length === 0)
+                abp.notify.info(app.localize('SplitRecordSelect'), app.localize('ValidationFailed'));
+            else if (selectedRecords && selectedRecords.length > 1)
+                abp.notify.info(app.localize('SingleSplit'), app.localize('ValidationFailed'));
+        }
+    },
+
 });

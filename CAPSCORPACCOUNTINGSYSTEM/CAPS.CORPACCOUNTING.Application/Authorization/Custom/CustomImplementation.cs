540f6235f3484f7497ece5cc46437d9e3de94682
rsharma <rsharma@capspayroll.com>
2016-07-23 10:41:42 +0000

    Added CustomImplementation.cs and UserPermissionsInput

---
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Application/Authorization/Custom/CustomImplementation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+using CAPS.CORPACCOUNTING.Authorization.Roles.Dto;
+using CAPS.CORPACCOUNTING.Authorization.Users;
+using CAPS.CORPACCOUNTING.Authorization.Users.Dto;
+using CAPS.CORPACCOUNTING.Dto;
+
+namespace CAPS.CORPACCOUNTING.Authorization.Custom
+{
+    /// <summary>
+    /// Partial class for UserAppService
+    /// </summary>
+    public partial class UserAppService : CORPACCOUNTINGAppServiceBase
+    {
+        /// <summary>
+        /// CreateOrUpdateUserUnit from User Permissions
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task UpdateUserUnit()
+        {
+            //await UserAppService. UpdateUserAsync(input);
+        }
+        
+    }
+}
---
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Application/Authorization/Users/Dto/UserPermissionsInput.cs
@@ -0,0 +1,36 @@
+﻿using Abp.Application.Services.Dto;
+using CAPS.CORPACCOUNTING.Authorization.Roles.Dto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPS.CORPACCOUNTING.Authorization.Users.Dto
+{
+    /// <summary>
+    /// UserPermissionsInput
+    /// </summary>
+    public class UserPermissionsInput : IInputDto
+    {
+        //public CreateOrUpdateUserInput UserInput { get; set; }
+        /// <summary>
+        /// UserId
+        /// </summary>
+        public long? UserId { get; set; }
+
+        /// <summary>
+        /// Role 
+        /// </summary>
+        [Required]
+        public RoleEditDto Role { get; set; }
+
+        // <summary>
+        /// GrantedPermissionNames
+        /// </summary>
+        [Required]
+        public List<string> GrantedPermissionNames { get; set; }
+
+    }
+}

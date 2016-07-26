5b2d542af02486ad201bd787dba9f010e753e203
skarupakula <skarupakula@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-07-26 09:31:09 +0000

    #re 24 Added Methods for Updating and Showing the CompanyLogo in Company

--- CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/Areas/Mpa/Controllers/OrganizationUnitsController.cs
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/Areas/Mpa/Controllers/OrganizationUnitsController.cs
@@ -51,49 +51,6 @@ namespace CAPS.CORPACCOUNTING.Web.Areas.Mpa.Controllers
         }
 
 
-        [AbpMvcAuthorize(AppPermissions.Pages_Administration_OrganizationUnits_ManageOrganizationTree)]
-        public JsonResult UpdateOrganizationPicture()
-        {
-            try
-            {
-                //Check input
-                if (Request.Files.Count <= 0 || Request.Files[0] == null)
-                {
-                    throw new UserFriendlyException(L("ProfilePicture_Change_Error"));
-                }
-
-                var file = Request.Files[0];
-
-                if (file.ContentLength > 5242880) //1MB.
-                {
-                    throw new UserFriendlyException(L("ProfilePicture_Warn_SizeLimit"));
-                }
-
-                //Check file type & format
-                var fileImage = Image.FromStream(file.InputStream);
-                if (!fileImage.RawFormat.Equals(ImageFormat.Jpeg) && !fileImage.RawFormat.Equals(ImageFormat.Png))
-                {
-                    throw new ApplicationException("Uploaded file is not an accepted image file !");
-                }
-
-                //Delete old temp profile pictures
-                AppFileHelper.DeleteFilesInFolderIfExists(_appFolders.TempFileDownloadFolder, "sumitOrgImage");
-
-                //Save new picture
-                var fileInfo = new FileInfo(file.FileName);
-                var tempFileName = "sumitOrgImage" +  fileInfo.Extension;
-                var tempFilePath = Path.Combine(_appFolders.TempFileDownloadFolder, tempFileName);
-                file.SaveAs(tempFilePath);
-
-                using (var bmpImage = new Bitmap(tempFilePath))
-                {
-                    return Json(new MvcAjaxResponse(new { fileName = tempFileName, width = bmpImage.Width, height = bmpImage.Height }));
-                }
-            }
-            catch (UserFriendlyException ex)
-            {
-                return Json(new MvcAjaxResponse(new ErrorInfo(ex.Message)));
-            }
-        }
+        
     }
 }
--- CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/Areas/Mpa/Controllers/TenantsController.cs
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/Areas/Mpa/Controllers/TenantsController.cs
@@ -1,14 +1,27 @@
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Abp.Application.Services.Dto;
+using Abp.UI;
+using Abp.Web.Models;
 using Abp.Web.Mvc.Authorization;
+using Abp.Web.Mvc.Models;
 using CAPS.CORPACCOUNTING.Authorization;
 using CAPS.CORPACCOUNTING.Editions;
+using CAPS.CORPACCOUNTING.IO;
 using CAPS.CORPACCOUNTING.MultiTenancy;
 using CAPS.CORPACCOUNTING.Web.Areas.Mpa.Models.Tenants;
 using CAPS.CORPACCOUNTING.Web.Controllers;
+using Abp.Auditing;
+using Abp.Domain.Repositories;
+using Abp.Runtime.Session;
+using CAPS.CORPACCOUNTING.Net.MimeTypes;
+using CAPS.CORPACCOUNTING.Storage;
 
 namespace CAPS.CORPACCOUNTING.Web.Areas.Mpa.Controllers
 {
@@ -18,15 +31,24 @@ namespace CAPS.CORPACCOUNTING.Web.Areas.Mpa.Controllers
         private readonly ITenantAppService _tenantAppService;
         private readonly TenantManager _tenantManager;
         private readonly IEditionAppService _editionAppService;
+        private readonly IAppFolders _appFolders;
+        private readonly IBinaryObjectManager _binaryObjectManager;
+        private readonly TenantExtendedUnitManager _tenantExtendedUnitManager;
+        protected IRepository<TenantExtendedUnit> _tenantExtendedUnitRepository { get; private set; }
+
+
 
         public TenantsController(
             ITenantAppService tenantAppService, 
             TenantManager tenantManager, 
-            IEditionAppService editionAppService)
+            IEditionAppService editionAppService, IAppFolders appFolders, IBinaryObjectManager binaryObjectManager, TenantExtendedUnitManager tenantExtendedUnitManager)
         {
             _tenantAppService = tenantAppService;
             _tenantManager = tenantManager;
             _editionAppService = editionAppService;
+            _appFolders = appFolders;
+            _binaryObjectManager = binaryObjectManager;
+            _tenantExtendedUnitManager = tenantExtendedUnitManager;
         }
 
         public ActionResult Index()
@@ -62,6 +84,86 @@ namespace CAPS.CORPACCOUNTING.Web.Areas.Mpa.Controllers
             var viewModel = new TenantFeaturesEditViewModel(tenant, output);
 
             return PartialView("_FeaturesModal", viewModel);
+        }
+
+
+        /// <summary>
+        /// Sumit Code to upload the CompanyLogo
+        /// </summary>
+        /// <returns></returns>
+        [AbpMvcAuthorize(AppPermissions.Pages_Tenants)]
+        public JsonResult UploadCompanyLogo()
+        {
+            try
+            {
+                //Check input
+                if (Request.Files.Count <= 0 || Request.Files[0] == null)
+                {
+                    throw new UserFriendlyException(L("ProfilePicture_Change_Error"));
+                }
+
+                var file = Request.Files[0];
+
+                if (file.ContentLength > 5242880) //1MB.
+                {
+                    throw new UserFriendlyException(L("ProfilePicture_Warn_SizeLimit"));
+                }
+
+                //Check file type & format
+                var fileImage = Image.FromStream(file.InputStream);
+                if (!fileImage.RawFormat.Equals(ImageFormat.Jpeg) && !fileImage.RawFormat.Equals(ImageFormat.Png))
+                {
+                    throw new ApplicationException("Uploaded file is not an accepted image file !");
+                }
+
+                //Delete old temp profile pictures
+                AppFileHelper.DeleteFilesInFolderIfExists(_appFolders.TempFileDownloadFolder, "sumitOrgImage");
+
+                //Save new picture
+                var fileInfo = new FileInfo(file.FileName);
+                var tempFileName = "sumitOrgImage" + fileInfo.Extension;
+                var tempFilePath = Path.Combine(_appFolders.TempFileDownloadFolder, tempFileName);
+                file.SaveAs(tempFilePath);
+
+                using (var bmpImage = new Bitmap(tempFilePath))
+                {
+                    return Json(new MvcAjaxResponse(new { fileName = tempFileName, width = bmpImage.Width, height = bmpImage.Height }));
+                }
+            }
+            catch (UserFriendlyException ex)
+            {
+                return Json(new MvcAjaxResponse(new ErrorInfo(ex.Message)));
+            }
+        }
+
+        /// <summary>
+        /// To get the CompanyLogo
+        /// </summary>
+        /// <returns></returns>
+
+        [DisableAuditing]
+        public async Task<ActionResult> ShowCompanyLogo()
+        {
+            FileResult picture;
+            var tenantExtenedUnit = await _tenantExtendedUnitRepository.FirstOrDefaultAsync(p=>p.TenantId==AbpSession.GetTenantId());
+            if (ReferenceEquals(tenantExtenedUnit.Logo,null))
+            {
+                    picture = GetDefaultCompanyLogo();
+            }
+            else
+            {
+                picture = tenantExtenedUnit.Logo!= null ? File(tenantExtenedUnit.Logo, MimeTypeNames.ImageJpeg) : GetDefaultCompanyLogo();
+            }
+            var contentType = picture.ContentType;
+            var fileContent = ((System.Web.Mvc.FileContentResult)picture).FileContents;
+
+            var base64String = Convert.ToBase64String(fileContent);
+            return Json((new { image = base64String, contentType = contentType, success = true }), JsonRequestBehavior.AllowGet);
+        }
+
+        private FileResult GetDefaultCompanyLogo()
+        {
+            return File(Server.MapPath("~/Common/Images/default-profile-picture.png"), MimeTypeNames.ImagePng);
         }
     }
 }

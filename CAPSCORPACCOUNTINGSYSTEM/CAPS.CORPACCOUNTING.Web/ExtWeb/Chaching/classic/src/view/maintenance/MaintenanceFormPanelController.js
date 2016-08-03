55982d4adc02fdf43aba2b445aa47bb0e4c0c7fa
kshahu <kshahu@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-08-03 04:43:52 +0000

    resolved the maintenance issue

--- CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/ExtWeb/Chaching/classic/src/view/maintenance/MaintenanceFormPanelController.js
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/ExtWeb/Chaching/classic/src/view/maintenance/MaintenanceFormPanelController.js
@@ -6,39 +6,46 @@
         view = me.getView();
         var cacheView = view.down('dataview[itemId=cacheDataView]');
         if (cacheView) {
-            var cashStore = cacheView.getStore();
-            if (cashStore && !cashStore.isLoaded())
-                cashStore.load();
+            var cacheStore = cacheView.getStore();
+            if (cacheStore && !cacheStore.isLoaded())
+                cacheStore.load();
         }
         var weblogView = view.down('dataview[itemId=webLogView]');
         if (weblogView) {            
-            this.loadWebLogView(weblogView);
+            this.loadWebLogView(weblogView, false);
         }
     },
 
-    loadWebLogView: function (view) {
-        var webLogStore = view.getStore();
+    loadWebLogView: function (view, isRefreshBtnClick) {
+        var webLogStore = view.getStore(),
+            me = this;
         if (webLogStore && !webLogStore.isLoaded()) {
-            webLogStore.load(function(records, operation, success) {
-                    Ext.each(records,
-                        function(record) {
-                            record.data = '<span class="log-line">' +
-                                record.data
-                                .replace('DEBUG', '<span class="label" style="background-color:#777;">DEBUG</span>')
-                                .replace('INFO', '<span class="label" style="background-color:#5bc0de;">INFO</span>')
-                                .replace('WARN', '<span class="label" style="background-color:#f0ad4e;">WARN</span>')
-                                .replace('ERROR', '<span class="label" style="background-color:#d9534f;">ERROR</span>')
-                                .replace('FATAL',
-                                    '<span class="label" style="background-color:#1ef7b8;">FATAL</span>') +
-                                '</span>';
-                        });
-                    if (view.getStore()) {
-                        view.getStore().removeAll();
-                        view.getStore().loadData(records);
-                    }
-
-                });
+            me.loadWebLogStore(webLogStore, view);
+        } else if (isRefreshBtnClick) {
+            me.loadWebLogStore(webLogStore, view);
         }
+    },
+
+    loadWebLogStore: function (webLogStore, view) {
+        webLogStore.load(function (records, operation, success) {
+            Ext.each(records,
+                function (record) {
+                    record.data = '<span class="log-line">' +
+                        record.data
+                        .replace('DEBUG', '<span class="label" style="background-color:#777;">DEBUG</span>')
+                        .replace('INFO', '<span class="label" style="background-color:#5bc0de;">INFO</span>')
+                        .replace('WARN', '<span class="label" style="background-color:#f0ad4e;">WARN</span>')
+                        .replace('ERROR', '<span class="label" style="background-color:#d9534f;">ERROR</span>')
+                        .replace('FATAL',
+                            '<span class="label" style="background-color:#1ef7b8;">FATAL</span>') +
+                        '</span>';
+                });
+            if (view.getStore()) {
+                view.getStore().removeAll();
+                view.getStore().loadData(records);
+            }
+
+        });
     },
 
     onMaintenanceResize: function (formPanel, newWidth, newHeight, oldWidth, oldHeight) {
@@ -93,7 +100,8 @@
         });
     },
 
-    buttonClicked: function (view, record, item, index, e, eOpts) {        
+    buttonClicked: function (view, record, item, index, e, eOpts) {
+        var me = this;
         if (item.name == "downloadAllButton") {
             Ext.Ajax.request({                
                     url: abp.appPath + 'api/services/app/webLog/DownloadWebLogs',
@@ -110,7 +118,7 @@
         }
         else {       
            if (item.name == "refreshButton") {
-               this.loadWebLogView(view);
+               me.loadWebLogView(view, true);
             }
         }
     }

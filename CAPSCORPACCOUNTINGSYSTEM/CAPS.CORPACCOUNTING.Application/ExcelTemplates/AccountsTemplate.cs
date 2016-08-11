9b6a45889721c217123bae65ec270e625bcddf9c
sgelli <sgelli@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-08-11 09:26:20 +0000

    AccountsTemplate changes

--- CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Application/ExcelTemplates/AccountsTemplate.cs
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Application/ExcelTemplates/AccountsTemplate.cs
@@ -1,10 +1,7 @@
 ﻿using CAPS.CORPACCOUNTING.DataExporting.Excel.EpPlus;
-using Abp.Runtime.Session;
-using Abp.Timing.Timezone;
 using CAPS.CORPACCOUNTING.Dto;
 using CAPS.CORPACCOUNTING.Masters;
 using CAPS.CORPACCOUNTING.Helpers;
-using CAPS.CORPACCOUNTING.Accounting;
 using CAPS.CORPACCOUNTING.Accounts;
 using System.Threading.Tasks;
 using CAPS.CORPACCOUNTING.Masters.Dto;
@@ -14,8 +11,9 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
     /// <summary>
     /// 
     /// </summary>
-    public class AccountsTemplate : EpPlusExcelExporterBase, ITemplate{
-        
+    public class AccountsTemplate : EpPlusExcelExporterBase, ITemplate
+    {
+
         private readonly IAccountUnitAppService _accountUnitAppService;
         private readonly int startRowIndex = 2;
         private readonly int endRowIndex = 3000;
@@ -27,7 +25,7 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
         public AccountsTemplate(
              AccountUnitAppService accountUnitAppService)
         {
-           
+
             _accountUnitAppService = accountUnitAppService;
         }
 
@@ -37,17 +35,30 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
         /// <returns></returns>
         public async Task<FileDto> DownLoadTemplate(int coaId)
         {
-
+            //Get all List Information
             var classificationlist = await _accountUnitAppService.GetTypeOfAccountList();
             var currencylist = await _accountUnitAppService.GetTypeOfCurrencyList();
             var consolidationList = EnumList.GetTypeofConsolidationList();
             var typeOfCurrencyRateList = await _accountUnitAppService.GetTypeOfCurrencyRateList();
             var linkedAccountList = await _accountUnitAppService.GetLinkAccountListByCoaId(new AutoSearchInput() { Id = coaId });
+            var booleanList = ExcelHelper.GetBooleanList();
+
+
             return CreateExcelPackage(
                 "AccountTemplate.xlsx",
                 excelPackage =>
                 {
                     var sheet = excelPackage.Workbook.Worksheets.Add(L("AccountsTemplate"));
+                    var listDataSheet = excelPackage.Workbook.Worksheets.Add(L("DropDownListInformation"));
+
+                    ExcelHelper.AddListDataIntoWorkSheet(listDataSheet, classificationlist, L("Classification"), "A");
+                    ExcelHelper.AddListDataIntoWorkSheet(listDataSheet, currencylist, L("Currency"), "B");
+                    ExcelHelper.AddListDataIntoWorkSheet(listDataSheet, consolidationList, L("Consolidation"), "C");
+                    ExcelHelper.AddListDataIntoWorkSheet(listDataSheet, typeOfCurrencyRateList, L("RateTypeOverride"), "D");
+                    ExcelHelper.AddListDataIntoWorkSheet(listDataSheet, linkedAccountList, L("NewAccount"), "E");
+                    ExcelHelper.AddListDataIntoWorkSheet(listDataSheet, booleanList, L("Flags"), "F");
+
+                    //Create Header Row
                     AddHeader(
                         sheet,
                     L("AccountNumber"),
@@ -63,25 +74,17 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
                     L("JournalsAllowed")
                     );
 
-                    ExcelHelper.AddTemplateObjects(
-                            sheet, startRowIndex, endRowIndex,
-                           new ExcelFields[]
-                           {
-                            new ExcelFields(L("AccountNumber"),iseditable:true),
-                            new ExcelFields(L("Description"),iseditable:true),
-                            new ExcelFields(L("Classification"),type:"dropdown",list:classificationlist),
-                            new ExcelFields(L("Consolidation"),type:"dropdown",list:consolidationList),
-                            new ExcelFields(L("Currency"),type:"dropdown",list:currencylist),
-                            new ExcelFields(L("NewAccount"),iseditable:true,list:linkedAccountList),
-                            new ExcelFields(L("Multi-CurrencyReval"),type:"dropdown",list:ExcelHelper.GetBooleanList()),
-                            new ExcelFields(L("RateTypeOverride"),type:"dropdown",list:typeOfCurrencyRateList),
-                            new ExcelFields(L("EliminationAccount"),type:"dropdown",list:ExcelHelper.GetBooleanList()),
-                            new ExcelFields(L("RollUpAccount"),type:"dropdown",list:ExcelHelper.GetBooleanList()),
-                            new ExcelFields(L("JournalsAllowed"),type:"dropdown",list:ExcelHelper.GetBooleanList())
-                           }
 
-                    );
-
+                    //reference list columns to Excel Sheet
+                    ExcelHelper.AddDropDownValidationToSheet(sheet, ExcelHelper.GetDropDownListFormula(L("DropDownListInformation"), "A", 2, classificationlist.Count + 2), startRowIndex, endRowIndex, 3);
+                    ExcelHelper.AddDropDownValidationToSheet(sheet, ExcelHelper.GetDropDownListFormula(L("DropDownListInformation"), "C", 2, consolidationList.Count + 2), startRowIndex, endRowIndex, 4);
+                    ExcelHelper.AddDropDownValidationToSheet(sheet, ExcelHelper.GetDropDownListFormula(L("DropDownListInformation"), "B", 2, currencylist.Count + 2), startRowIndex, endRowIndex, 5);
+                    ExcelHelper.AddDropDownValidationToSheet(sheet, ExcelHelper.GetDropDownListFormula(L("DropDownListInformation"), "E", 2, linkedAccountList.Count + 2), startRowIndex, endRowIndex, 6);
+                    ExcelHelper.AddDropDownValidationToSheet(sheet, ExcelHelper.GetDropDownListFormula(L("DropDownListInformation"), "F", 2, booleanList.Count + 2), startRowIndex, endRowIndex, 7);
+                    ExcelHelper.AddDropDownValidationToSheet(sheet, ExcelHelper.GetDropDownListFormula(L("DropDownListInformation"), "F", 2, booleanList.Count + 2), startRowIndex, endRowIndex, 8);
+                    ExcelHelper.AddDropDownValidationToSheet(sheet, ExcelHelper.GetDropDownListFormula(L("DropDownListInformation"), "F", 2, booleanList.Count + 2), startRowIndex, endRowIndex, 9);
+                    ExcelHelper.AddDropDownValidationToSheet(sheet, ExcelHelper.GetDropDownListFormula(L("DropDownListInformation"), "F", 2, booleanList.Count + 2), startRowIndex, endRowIndex, 10);
+                    ExcelHelper.AddDropDownValidationToSheet(sheet, ExcelHelper.GetDropDownListFormula(L("DropDownListInformation"), "F", 2, booleanList.Count + 2), startRowIndex, endRowIndex, 11);
 
                 });
         }
--- CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Application/ExcelTemplates/ExcelHelper.cs
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Application/ExcelTemplates/ExcelHelper.cs
@@ -4,16 +4,15 @@ using System.Drawing;
 using OfficeOpenXml.Style;
 using System.Collections.Generic;
 using OfficeOpenXml.DataValidation.Contracts;
-using System.Threading.Tasks;
-using System;
 using System.Data;
-using Abp.Collections.Extensions;
 using Abp.Application.Services.Dto;
 using OfficeOpenXml.DataValidation;
 
 namespace CAPS.CORPACCOUNTING.ExcelTemplates
 {
-
+    /// <summary>
+    /// 
+    /// </summary>
     public class ExcelFields
     {
         public string FieldValue { get; set; }
@@ -37,12 +36,6 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
             FieldValue = fieldvalue;
             ColumnName = columnName;
         }
-
-        //public ExcelFields(string columnName, bool iseditable = false)
-        //{
-        //    IsEditable = iseditable;
-        //    ColumnName = columnName;
-        //}
     }
 
 
@@ -53,6 +46,7 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
 
     public static class ExcelHelper
     {
+        const string listDataExcelSheet = "DropDownListInformation";
         const string strOutPutDir = @"C:\EPPlus\Excel";
         const string strPassword = @"Sumit";
 
@@ -121,7 +115,7 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
         }
 
         /// <summary>
-        /// 
+        /// Fill Dropdown List to Cells in ExcelSheet
         /// </summary>
         public static void AddTemplateObjects(ExcelWorksheet sheet, int startRowIndex, int endRowIndex, params ExcelFields[] propertySelectors)
         {
@@ -160,17 +154,7 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
 
 
         /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="cell"></param>
-        public static void CellEditable(ExcelRange cell)
-        {
-            cell.Style.Fill.PatternType = ExcelFillStyle.Solid;
-            cell.Style.Fill.BackgroundColor.SetColor(Color.LightGray);
-        }
-
-        /// <summary>
-        /// 
+        /// Get Boolean values as List
         /// </summary>
         /// <returns></returns>
         public static List<NameValueDto> GetBooleanList()
@@ -183,17 +167,44 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
             });
         }
 
-        private static void AddListDataIntoWorkSheet(ExcelWorksheet sheet, List<NameValueDto> list, string listName, string cellColumn)
+        /// <summary>
+        /// List Data is Added to Excel Sheet
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <param name="list"></param>
+        /// <param name="listName"></param>
+        /// <param name="cellColumn"></param>
+        public static void AddListDataIntoWorkSheet(ExcelWorksheet sheet, List<NameValueDto> list, string listName, string cellColumn)
         {
             int j = 2;
-            //var sheet = package.Workbook.Worksheets.Add("list formula");
             sheet.Cells[cellColumn + "1"].Style.Font.Bold = true;
             sheet.Cells[cellColumn + "1"].Value = listName;
+            if (list.Count > 0)
+            {
+                for (int i = 0; i <= list.Count - 1; i++)
+                {
+                    sheet.Cells[cellColumn + (j + i)].Value = list[i].Name;
+                }
+            }
+        }
+
+        /// <summary>
+        ///  List Data is Added to Excel Sheet
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <param name="list"></param>
+        /// <param name="listName"></param>
+        /// <param name="columnNumber"></param>
+        public static void AddListDataIntoWorkSheet(ExcelWorksheet sheet, List<NameValueDto> list, string listName, int columnNumber)
+        {
+            int j = 2;
+
+            sheet.Cells[1, columnNumber].Style.Font.Bold = true;
+            sheet.Cells[1, columnNumber].Value = listName;
 
             for (int i = 0; i <= list.Count - 1; i++)
             {
-                j = j + i;
-                sheet.Cells[cellColumn + j].Value = list[i].Name;
+                sheet.Cells[(j + i), columnNumber].Value = list[i].Name;
             }
 
 
@@ -208,7 +219,15 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
             //validation.Formula.ExcelFormula = "B2:B4";
         }
 
-        private static void AddDropDownValidationToSheet(ExcelWorksheet sheet, string excelFormula,int startRowIndex, int endRowIndex,int excelColumn)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <param name="excelFormula"></param>
+        /// <param name="startRowIndex"></param>
+        /// <param name="endRowIndex"></param>
+        /// <param name="excelColumn"></param>
+        public static void AddDropDownValidationToSheet(ExcelWorksheet sheet, string excelFormula, int startRowIndex, int endRowIndex, int excelColumn)
         {
             var validation = sheet.Cells[startRowIndex, excelColumn, endRowIndex, excelColumn].DataValidation.AddListDataValidation();
             validation.ShowErrorMessage = true;
@@ -218,9 +237,60 @@ namespace CAPS.CORPACCOUNTING.ExcelTemplates
             validation.Formula.ExcelFormula = excelFormula;
         }
 
-        private static string GetDropDownListFormula(string cellColumn, int lstFromRange, int lstEndRange)
+
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public static void AddTemplateObjects(ExcelPackage excelPackage,ExcelWorksheet sheet, int startRowIndex, int endRowIndex, params ExcelFields[] propertySelectors)
+        //{
+        //    var listDataSheet = excelPackage.Workbook.Worksheets.Add(listDataExcelSheet);
+        //    for (int j = 0; j <= propertySelectors.Length - 1; j++)
+        //    {
+        //        var columnName = propertySelectors[j].ColumnName;
+        //        if (propertySelectors[j].Type == "dropdown")
+        //        {
+        //            if (propertySelectors[j].List.Count > 0)
+        //            {
+        //                AddListDataIntoWorkSheet(listDataSheet, propertySelectors[j].List, columnName, j + 1);
+        //                var excelFormula = GetDropDownListFormula(listDataExcelSheet, "A", 2, propertySelectors[j].List.Count + 2);
+
+
+        //                var validation = sheet.Cells[startRowIndex, j + 1, endRowIndex, j + 1].DataValidation.AddListDataValidation();
+        //                validation.ShowErrorMessage = true;
+        //                validation.ErrorStyle = ExcelDataValidationWarningStyle.warning;
+        //                validation.ErrorTitle = "An invalid value was entered";
+        //                validation.Error = "Select a value from the list";
+        //                validation.Formula.ExcelFormula = excelFormula;
+        //            }
+
+        //        }
+        //        else if (!propertySelectors[j].IsEditable)
+        //        {
+        //            sheet.Cells[startRowIndex, j + 1, endRowIndex, j + 1].Style.Locked = true;
+        //            sheet.Cells[startRowIndex, j + 1, endRowIndex, j + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+        //            sheet.Cells[startRowIndex, j + 1, endRowIndex, j + 1].Style.Fill.BackgroundColor.SetColor(Color.LightGray);
+        //        }
+        //        else
+        //        {
+        //            sheet.Cells[startRowIndex, j + 1, endRowIndex, j + 1].Value = "";
+        //        }
+        //    }
+
+
+        //}
+
+        /// <summary>
+        /// =Sheet2!$A$1:$A$4
+        /// Refer other sheet data as Dropdown list values in Cells
+        /// </summary>
+        /// <param name="cellColumn"></param>
+        /// <param name="lstFromRange"></param>
+        /// <param name="lstEndRange"></param>
+        /// <param name="sheetName"></param>
+        /// <returns></returns>
+        public static string GetDropDownListFormula(string sheetName, string cellColumn, int lstFromRange, int lstEndRange)
         {
-            return cellColumn + lstFromRange + ":" + cellColumn + lstEndRange;
+            return "'"+sheetName+"'" + "!" + "$" + cellColumn + "$" + lstFromRange + ":" + "$" + cellColumn + "$" + lstEndRange;
         }
 
         /// <summary>

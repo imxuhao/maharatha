5f2d893924f905c7fb4fa8248d3723b8a6444d36
kgarad <kgarad@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-08-18 22:40:44 +0000

    Added always visible column header trigger for grid.

--- CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/ExtWeb/Chaching/classic/src/view/receivables/customers/CustomerForm.js
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/ExtWeb/Chaching/classic/src/view/receivables/customers/CustomerForm.js
@@ -1,4 +1,5 @@
-﻿Ext.define('Chaching.view.receivables.customers.CustomerForm', {
+﻿Ext.define('Chaching.view.receivables.customers.CustomerForm',
+{
     extend: 'Chaching.view.common.form.ChachingFormPanel',
     alias: ['widget.receivables.customers.create', 'widget.receivables.customers.edit'],
     requires: [
@@ -16,216 +17,168 @@
     hideDefaultButtons: false,
     autoScroll: true,
     border: false,
-    showFormTitle: true,
+    showFormTitle: false,
     displayDefaultButtonsCenter: true,
-
-
-    //titleConfig: {
-    //    title: abp.localization.localize("CreatingNewCustomers").initCap()
-    //},
-    items: [{
-        xtype: 'tabpanel',
-        ui: 'formTabPanels',
-        items: [
+    items: [
         {
-            title: abp.localization.localize("ContactInformation"),
-            iconCls: 'fa fa-gear',
-            layout: 'column',
-            items: [{
-                //xtype: 'fieldset',
-                //ui: 'transparentFieldSet',
-                //title: abp.localization.localize("GeneralInformation"),
-                //collapsible: true,
-                //layout: 'column',
-                //items: [{
-                    columnWidth: .5,
-                    padding: '20 10 0 20',
-                    defaults: {
-                        // labelAlign: 'top',
-                        labelWidth: 140,
-                        blankText: app.localize('MandatoryToolTipText')
-                    },
-                    items: [{
-                        xtype: 'hiddenfield',
-                        name: 'customerId',
-                        value: 0
-                    }, {
-                        xtype: 'textfield',
-                        name: 'lastName',
-                        itemId: 'lastName',
-                        allowBlank: false,
-                        fieldLabel: app.localize('AgencyCompanyName'),
-                        width: '100%',
-                        ui: 'fieldLabelTop',
-                        emptyText: app.localize('MandatoryField')
-                    },
-                         {
-                             xtype: 'textfield',
-                             name: 'customerNumber',
-                             itemId: 'customerNumber',
-                             fieldLabel: app.localize('CustomerNumber'),
-                             width: '100%',
-                             ui: 'fieldLabelTop'
-                         }
+            xtype: 'tabpanel',
+            ui: 'formTabPanels',
+            items: [
+                {
+                    title: abp.localization.localize("ContactInformation"),
+                    iconCls: 'fa fa-gear',
+                    layout: 'column',
+                    items: [
+                        {
+                            columnWidth: .5,
+                            padding: '20 10 0 20',
+                            defaults: {
+                                labelWidth: 140,
+                                blankText: app.localize('MandatoryToolTipText')
+                            },
+                            items: [
+                                {
+                                    xtype: 'hiddenfield',
+                                    name: 'customerId',
+                                    value: 0
+                                }, {
+                                    xtype: 'textfield',
+                                    name: 'lastName',
+                                    itemId: 'lastName',
+                                    allowBlank: false,
+                                    fieldLabel: app.localize('AgencyCompanyName'),
+                                    width: '100%',
+                                    ui: 'fieldLabelTop',
+                                    emptyText: app.localize('MandatoryField')
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    name: 'customerNumber',
+                                    itemId: 'customerNumber',
+                                    fieldLabel: app.localize('CustomerNumber'),
+                                    width: '100%',
+                                    ui: 'fieldLabelTop'
+                                }
+                            ]
+                        },
+                        {
+                            columnWidth: .5,
+                            padding: '20 10 0 20',
+                            defaults: {
+                                blankText: app.localize('MandatoryToolTipText')
+                            },
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    name: 'firstName',
+                                    itemId: 'firstName',
+                                    fieldLabel: app.localize('FirstName').initCap(),
+                                    width: '100%',
+                                    ui: 'fieldLabelTop'
+                                },
+                                {
+                                    xtype: 'checkbox',
+                                    name: 'isActive',
+                                    itemId: 'isActive',
+                                    labelAlign: 'right',
+                                    inputValue: true,
+                                    checked: true,
+                                    ui: 'default',
+                                    boxLabelCls: 'checkboxLabel',
+                                    boxLabel: app.localize('Active')
+                                }
+                            ]
+                        },
+                        {
+                            columnWidth: 1,
+                            defaults: {
+                                labelAlign: 'top',
+                                blankText: app.localize('MandatoryToolTipText')
+                            },
+                            layout: 'fit',
+                            items: [
+                                {
+                                    xtype: 'address',
+                                    itemId: 'addressGrid',
+                                    layout: 'fit',
+                                    width: '100%'
+                                }
+                            ]
+                        }
                     ]
                 },
                 {
-                    columnWidth: .5,
-                    padding: '20 10 0 20',
-                    defaults: {
-                        // labelAlign: 'top',
-                        blankText: app.localize('MandatoryToolTipText')
-                    },
-                    items: [{
-                        xtype: 'textfield',
-                        name: 'firstName',
-                        itemId: 'firstName',
-                        fieldLabel: app.localize('FirstName').initCap(),
-                        width: '100%',
-                        ui: 'fieldLabelTop'
-                    },
-                    {
-                        xtype: 'checkbox',
-                        name: 'isActive',
-                        itemId: 'isActive',
-                        labelAlign: 'right',
-                        inputValue: true,
-                        checked: true,
-                        ui: 'default',
-                        boxLabelCls: 'checkboxLabel',
-                        boxLabel: app.localize('Active')
-                    }
-                    //, {
-                    //    xtype: 'textfield',
-                    //    name: 'billingAccount',
-                    //    itemId: 'billingAccount',
-                    //    fieldLabel: app.localize('BillingAccount').initCap(),
-                    //    width: '100%',
-                    //    ui: 'fieldLabelTop'
-                    //}
+                    title: abp.localization.localize("CreditInformation"),
+                    iconCls: 'fa fa-gear',
+                    layout: 'column',
+                    items: [
+                        {
+                            columnWidth: .5,
+                            padding: '20 10 0 20',
+                            defaults: {
+                                labelWidth: 120
+                            },
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    name: 'creditLimit',
+                                    itemId: 'creditLimit',
+                                    fieldLabel: app.localize('CreditLimit').initCap(),
+                                    width: '100%',
+                                    ui: 'fieldLabelTop'
+                                },
+                                {
+                                    xtype: 'combobox',
+                                    name: 'customerPayTermsId',
+                                    fieldLabel: app.localize('PaymentTerms').initCap(),
+                                    width: '100%',
+                                    ui: 'fieldLabelTop',
+                                    displayField: 'paymentTerms',
+                                    valueField: 'paymentTermsId',
+                                    emptyText: app.localize('SelectOption')
 
-                    ]
-                }
-
-                ,
-                    //{
-                    //    columnWidth: .33,
-                    //    padding: '20 10 0 20',
-                    //    defaults: {
-                    //        // labelAlign: 'top',
-                    //        blankText: app.localize('MandatoryToolTipText')
-                    //    },
-                    //    items: [
-                    //    {
-                    //        xtype: 'checkbox',
-                    //        name: 'isActive',
-                    //        itemId: 'isActive',
-                    //        labelAlign: 'right',
-                    //        inputValue: true,
-                    //        checked: true,
-                    //        ui: 'default',
-                    //        boxLabelCls: 'checkboxLabel',
-                    //        boxLabel: app.localize('Active')
-                    //    }
-                    //    ]
-                    //},
-
-
-                    {
-                        columnWidth: 1,
-                        padding: '20 10 0 20',
-                        defaults: {
-                            labelAlign: 'top',
-                            blankText: app.localize('MandatoryToolTipText')
+                                }
+                            ]
                         },
-                        items: [{
-                            xtype: 'address',
-                            itemId: 'addressGrid',
-                            layout: 'fit',
-                            width: '100%'
-                        }]
-                    }]
-            //}]
-        },
-        {
-            title: abp.localization.localize("CreditInformation"),
-            iconCls: 'fa fa-gear',
-            layout: 'column',
-            items: [
-              {
-                  //xtype: 'fieldset',
-                  //collapsible: true,
-                  //title: abp.localization.localize("PaymentInformation"),
-                  //ui: 'transparentFieldSet',
-                  //layout: 'column',
-                  //items: [{
-                    columnWidth: .5,
-                    padding: '20 10 0 20',
-                    defaults: {
-                    labelWidth: 120
-                //labelAlign: 'top',
-                //blankText: app.localize('MandatoryToolTipText')
-            },
-            items: [
-                 {
-                     xtype: 'textfield',
-                     name: 'creditLimit',
-                     itemId: 'creditLimit',
-                     fieldLabel: app.localize('CreditLimit').initCap(),
-                     width: '100%',
-                     ui: 'fieldLabelTop'
-                 },
+                        {
+                            columnWidth: .5,
+                            padding: '20 10 0 20',
+                            defaults: {
+                                labelWidth: 120
+                            },
+                            items: [
+                                {
+                                    xtype: 'combobox',
+                                    name: 'typeofPaymentMethodId',
+                                    fieldLabel: app.localize('PaymentMethods').initCap(),
+                                    width: '100%',
+                                    ui: 'fieldLabelTop',
+                                    displayField: 'typeofPaymentMethod',
+                                    valueField: 'typeofPaymentMethodId',
+                                    emptyText: app.localize('SelectOption')
+                                    //,
+                                    //bind: {
+                                    //    store: '{paymentTermsList}'
+                                    //}
+                                },
+                                {
+                                    xtype: 'combobox',
+                                    name: 'salesRepId',
+                                    fieldLabel: app.localize('SalesRep').initCap(),
+                                    width: '100%',
+                                    ui: 'fieldLabelTop',
+                                    displayField: 'salesRepName',
+                                    valueField: 'salesRepId',
+                                    emptyText: app.localize('SelectOption')
 
-              {
-                  xtype: 'combobox',
-                  name: 'customerPayTermsId',
-                  fieldLabel: app.localize('PaymentTerms').initCap(),
-                  width: '100%',
-                  ui: 'fieldLabelTop',
-                  displayField: 'paymentTerms',
-                  valueField: 'paymentTermsId',
-                  emptyText: app.localize('SelectOption')
+                                }
+                            ]
+                        }
+                    ]
 
-              }]
-        },
-                  {
-                      columnWidth: .5,
-                      padding: '20 10 0 20',
-                      defaults: {
-                          labelWidth: 120
-                          //labelAlign: 'top',
-                          //blankText: app.localize('MandatoryToolTipText')
-                      },
-                      items: [
-                 {
-                     xtype: 'combobox',
-                     name: 'typeofPaymentMethodId',
-                     fieldLabel: app.localize('PaymentMethods').initCap(),
-                     width: '100%',
-                     ui: 'fieldLabelTop',
-                     displayField: 'typeofPaymentMethod',
-                     valueField: 'typeofPaymentMethodId',
-                     emptyText: app.localize('SelectOption')
-                     //,
-                     //bind: {
-                     //    store: '{paymentTermsList}'
-                     //}
-                 },
-              {
-                  xtype: 'combobox',
-                  name: 'salesRepId',
-                  fieldLabel: app.localize('SalesRep').initCap(),
-                  width: '100%',
-                  ui: 'fieldLabelTop',
-                  displayField: 'salesRepName',
-                  valueField: 'salesRepId',
-                  emptyText: app.localize('SelectOption')
+                }
+            ]
 
-              }]
-                  //}]
-              }]
-
-        }]
-
-    }]
+        }
+    ]
 });

9665fd65e00d7abcd5bbdbd9313eba30e8eb727b
skarupakula <skarupakula@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-08-24 06:17:07 +0000

    re #29 to access CustomAccountRepository.cs class changed from abstract to normal class.

--- CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.EntityFramework/EntityFramework/Repositories/CustomAccountRepository.cs
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.EntityFramework/EntityFramework/Repositories/CustomAccountRepository.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Abp.EntityFramework;
 using CAPS.CORPACCOUNTING.Masters;
 using CAPS.CORPACCOUNTING.Masters.CustomRepository;
-using CAPS.CORPACCOUNTING.Masters.Dto;
-using Z.EntityFramework.Extensions;
 
 namespace CAPS.CORPACCOUNTING.EntityFramework.Repositories
 {
-    public abstract class CustomAccountRepository : CORPACCOUNTINGRepositoryBase<AccountUnit, long>, ICustomAccountRepository
+    public class CustomAccountRepository : CORPACCOUNTINGRepositoryBase<AccountUnit, long>, ICustomAccountRepository
     {
         public CustomAccountRepository(IDbContextProvider<CORPACCOUNTINGDbContext> dbContextProvider)
         : base(dbContextProvider)

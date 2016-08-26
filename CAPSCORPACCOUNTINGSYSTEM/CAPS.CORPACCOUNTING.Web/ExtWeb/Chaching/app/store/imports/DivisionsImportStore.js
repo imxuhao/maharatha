1ac0cc084b9e87d844767bcd017b27520f70da15
skohley <skohley@54f6d4f6-8cd8-465c-a0f6-5787db647fd9>
2016-08-26 16:13:51 +0000

    #46: Integrate excel import plugin (Divisions) added missed store

---
+++ CAPSCORPACCOUNTINGSYSTEM/CAPS.CORPACCOUNTING.Web/ExtWeb/Chaching/app/store/imports/DivisionsImportStore.js
@@ -0,0 +1,41 @@
+﻿/**
+ * Local file store to import accounts.
+ */
+Ext.define('Chaching.store.imports.DivisionsImportStore', {
+    extend: 'Gearbox.data.file.Store',
+    fields: [
+    {
+        name: 'jobNumber',
+        type: 'string',
+        mapping: { source: 'Number' },
+        mandatory: true,
+        duplicate: true
+    },
+    {
+        name: 'caption',
+        type: 'string',
+        mapping: { source: 'Description' }
+    },
+    {
+        name: 'typeOfCurrencyId',
+        type: 'int',
+        mapping: { source: 'Currency' }
+    },
+    {
+        name: 'isActive',
+        type: 'boolean',
+        mapping: { source: 'Active' }
+    }, 
+    {
+        name: 'errorMessage',
+        type: 'string'
+    }],
+    autoGuessMapping: false,
+    proxy: {
+        type: 'file',
+        reader: {
+            type: 'file.xlsx'
+        },
+        writer: 'file.xlsx'
+    }
+});
